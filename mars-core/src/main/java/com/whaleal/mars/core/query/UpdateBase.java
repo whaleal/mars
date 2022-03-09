bf22297418237cd0d34719c2557887c984110e67
hbn.king <hbn.king@gmail.com>
2022-03-09 21:27:28 +0800

    add updatebase

---
+++ mars-core/src/main/java/com/whaleal/mars/core/query/UpdateBase.java
@@ -0,0 +1,99 @@
+package dev.morphia.query;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.lang.Nullable;
+import dev.morphia.Datastore;
+import dev.morphia.internal.PathTarget;
+import dev.morphia.mapping.Mapper;
+import dev.morphia.query.experimental.updates.UpdateOperator;
+import org.bson.Document;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+/**
+ * @param <T>
+ * @morphia.internal
+ */
+public abstract class UpdateBase<T> {
+
+    private final Query<T> query;
+    private final MongoCollection<T> collection;
+    private final Mapper mapper;
+    private final Class<T> type;
+    private final List<UpdateOperator> updates = new ArrayList<>();
+    private final Datastore datastore;
+
+    UpdateBase(Datastore datastore,
+               @Nullable MongoCollection<T> collection,
+               @Nullable Query<T> query,
+               Class<T> type,
+               List<UpdateOperator> updates) {
+        this.datastore = datastore;
+        this.mapper = datastore.getMapper();
+        this.type = type;
+        this.updates.addAll(updates);
+        this.query = query;
+        this.collection = collection;
+    }
+
+    @NotNull
+    static <T> List<T> coalesce(T first, T[] updates) {
+        List<T> operators = new ArrayList<>();
+        operators.add(first);
+        operators.addAll(asList(updates));
+        return operators;
+    }
+
+    /**
+     * Adds a new operator to this update operation.
+     *
+     * @param operator the new operator
+     * @morphia.internal
+     * @since 2.2
+     */
+    public void add(UpdateOperator operator) {
+        updates.add(operator);
+    }
+
+    /**
+     * @return the operations listed
+     */
+    public Document toDocument() {
+        final Operations operations = new Operations(datastore, mapper.getEntityModel(type));
+
+        for (UpdateOperator update : updates) {
+            PathTarget pathTarget = new PathTarget(mapper, mapper.getEntityModel(type), update.field(), true);
+            operations.add(update.operator(), update.toTarget(pathTarget));
+        }
+        return operations.toDocument();
+    }
+
+    @Override
+    public String toString() {
+        return toDocument().toString();
+    }
+
+    protected MongoCollection<T> getCollection() {
+        return collection;
+    }
+
+    protected Datastore getDatastore() {
+        return datastore;
+    }
+
+    protected Query<T> getQuery() {
+        return query;
+    }
+
+    /**
+     * @return the updates
+     * @morphia.internal
+     */
+    protected List<UpdateOperator> getUpdates() {
+        return updates;
+    }
+}
