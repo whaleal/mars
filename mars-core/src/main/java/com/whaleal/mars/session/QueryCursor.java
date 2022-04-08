e68b2b1a14b417057c678fb0b50210e2eb504a6d
hbn.king <hbn.king@gmail.com>
2022-04-08 23:32:00 +0800

    formate Code

--- mars-core/src/main/java/com/whaleal/mars/session/QueryCursor.java
+++ mars-core/src/main/java/com/whaleal/mars/session/QueryCursor.java
@@ -1,31 +1,31 @@
 /**
- *    Copyright 2020-present  Shanghai Jinmu Information Technology Co., Ltd.
- *
- *    This program is free software: you can redistribute it and/or modify
- *    it under the terms of the Server Side Public License, version 1,
- *    as published by Shanghai Jinmu Information Technology Co., Ltd.(The name of the development team is Whaleal.)
- *
- *
- *    This program is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    Server Side Public License for more details.
- *
- *    You should have received a copy of the Server Side Public License
- *    along with this program. If not, see
- *    <http://www.whaleal.com/licensing/server-side-public-license>.
- *
- *    As a special exception, the copyright holders give permission to link the
- *    code of portions of this program with the OpenSSL library under certain
- *    conditions as described in each individual source file and distribute
- *    linked combinations including the program with the OpenSSL library. You
- *    must comply with the Server Side Public License in all respects for
- *    all of the code used other than as permitted herein. If you modify file(s)
- *    with this exception, you may extend this exception to your version of the
- *    file(s), but you are not obligated to do so. If you do not wish to do so,
- *    delete this exception statement from your version. If you delete this
- *    exception statement from all source files in the program, then also delete
- *    it in the license file.
+ * Copyright 2020-present  Shanghai Jinmu Information Technology Co., Ltd.
+ * <p>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the Server Side Public License, version 1,
+ * as published by Shanghai Jinmu Information Technology Co., Ltd.(The name of the development team is Whaleal.)
+ * <p>
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * Server Side Public License for more details.
+ * <p>
+ * You should have received a copy of the Server Side Public License
+ * along with this program. If not, see
+ * <http://www.whaleal.com/licensing/server-side-public-license>.
+ * <p>
+ * As a special exception, the copyright holders give permission to link the
+ * code of portions of this program with the OpenSSL library under certain
+ * conditions as described in each individual source file and distribute
+ * linked combinations including the program with the OpenSSL library. You
+ * must comply with the Server Side Public License in all respects for
+ * all of the code used other than as permitted herein. If you modify file(s)
+ * with this exception, you may extend this exception to your version of the
+ * file(s), but you are not obligated to do so. If you do not wish to do so,
+ * delete this exception statement from your version. If you delete this
+ * exception statement from all source files in the program, then also delete
+ * it in the license file.
  */
 package com.whaleal.mars.session;
 
@@ -45,29 +45,27 @@ import java.util.NoSuchElementException;
  *            基于Document 的 进行转换
  */
 
-public class QueryCursor<T> implements MarsCursor<T> {
-    private final MongoCursor<T> wrapped;
+public class QueryCursor< T > implements MarsCursor< T > {
+    private final MongoCursor< T > wrapped;
 
-
-    private final Class<? super T> rawType;
+    private final Class< ? super T > rawType;
 
 
     /**
      * @param cursor the Iterator to use
      */
-    public QueryCursor(MongoCursor<T> cursor, Class<? super T> entityClass) {
-        wrapped = cursor;
-        rawType = entityClass;
-        if (wrapped == null) {
+    public QueryCursor( MongoCursor< T > cursor, Class< ? super T > entityClass ) {
+        this.wrapped = cursor;
+        this.rawType = entityClass;
+        if (this.wrapped == null) {
             throw new IllegalArgumentException("The wrapped cursor can not be null");
         }
-
     }
 
 
-    public QueryCursor(MongoCursor<T> cursor) {
-        wrapped = cursor;
-        rawType = null;
+    public QueryCursor( MongoCursor< T > cursor ) {
+        this.wrapped = cursor;
+        this.rawType = null;
         if (wrapped == null) {
             throw new IllegalArgumentException("The wrapped cursor can not be null");
         }
@@ -79,14 +77,14 @@ public class QueryCursor<T> implements MarsCursor<T> {
      *
      * @return the list of Entities
      */
-    public List<T> toList() {
-        final List<T> results = new ArrayList<>();
+    public List< T > toList() {
+        final List< T > results = new ArrayList<>();
         try {
-            while (wrapped.hasNext()) {
+            while (this.wrapped.hasNext()) {
                 results.add(next());
             }
         } finally {
-            wrapped.close();
+            this.wrapped.close();
         }
         return results;
     }
@@ -96,17 +94,17 @@ public class QueryCursor<T> implements MarsCursor<T> {
      */
     @Override
     public void close() {
-        if (wrapped != null) {
-            wrapped.close();
+        if (this.wrapped != null) {
+            this.wrapped.close();
         }
     }
 
     @Override
     public boolean hasNext() {
-        if (wrapped == null) {
+        if (this.wrapped == null) {
             return false;
         }
-        return wrapped.hasNext();
+        return this.wrapped.hasNext();
     }
 
     @Override
@@ -115,8 +113,7 @@ public class QueryCursor<T> implements MarsCursor<T> {
             throw new NoSuchElementException();
         }
 
-
-        return wrapped.next();
+        return this.wrapped.next();
 
     }
 
@@ -131,12 +128,12 @@ public class QueryCursor<T> implements MarsCursor<T> {
 
 
     public ServerCursor getServerCursor() {
-        return wrapped.getServerCursor();
+        return this.wrapped.getServerCursor();
     }
 
 
     public ServerAddress getServerAddress() {
-        return wrapped.getServerAddress();
+        return this.wrapped.getServerAddress();
     }
 
     @Override
