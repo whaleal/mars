6b62b1495a001402d9c1813eb0cc3d6b2cd55491
WhaleFalls <2450706719@qq.com>
2022-06-20 18:24:58 +0800

    query sort功能测试代码新增

---
+++ mars-core/src/main/java/com/whaleal/mars/core/query/Sort.java
@@ -0,0 +1,10 @@
+package com.whaleal.mars.core.query;
+
+/**
+ * @author lyz
+ * @description cha
+ * @date 2022-06-17 12:48
+ **/
+public class Sort extends com.whaleal.mars.core.aggregation.stages.Sort {
+
+}
