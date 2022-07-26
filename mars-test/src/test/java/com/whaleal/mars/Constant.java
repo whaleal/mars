7e1395a9308b0ea222bbad39b8034d0b23a75d17
WhaleFalls <2450706719@qq.com>
2022-07-26 22:43:49 +0800

    release 0.8.41 with spring boot version 2.5.1

--- mars-test/src/test/java/com/whaleal/mars/Constant.java
+++ mars-test/src/test/java/com/whaleal/mars/Constant.java
@@ -9,7 +9,7 @@ public class Constant {
     //针对与部分需要指定未admin库才能运行的command
 //    public final static String connectionAdminStr = "mongodb://root:123456@47.100.1.115:37001/admin?authSource=admin";
 //    public final static String connectionStr = "mongodb://127.0.0.1:6001,127.0.0.1:6002,127.0.0.1:6003/mars";
-    public final static String connectionStr = "mongodb://192.168.3.186:27017/mars";
+    public final static String connectionStr = "mongodb://127.0.0.1:27017/mars";
 
 
 
