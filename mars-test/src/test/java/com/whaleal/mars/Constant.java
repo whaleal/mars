f29229ad51fed8159080865713d9a9322ba86461
hbn.king <hbn.king@gmail.com>
2024-01-08 09:57:27 +0800

    localhost test

--- mars-test/src/test/java/com/whaleal/mars/Constant.java
+++ mars-test/src/test/java/com/whaleal/mars/Constant.java
@@ -3,9 +3,7 @@ package com.whaleal.mars;
 public class Constant {
 
 
-
-    public final static String connectionStr = "mongodb://root:jxg8JfyyFHxMVOo9RB77@172.234.85.21:27000,172.234.85.22:27000,172.234.85.23:27000/wh?replicaSet=appdb&authSource=admin";
-
+    public final static String connectionStr = "mongodb://localhost:6001,localhost:6002,localhost:6003/wh";
 
 
 }
